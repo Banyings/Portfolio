d5c6463db650ba49b4e10f2740511ab2b8e518eb
Hyppolite Banyingela <banyingelahyppo@gmail.com>
2024-11-25 18:05:58 -0700

    done

--- next.config.ts
+++ next.config.ts
@@ -1,33 +1,11 @@
-// import type { NextConfig } from "next";
+import type { NextConfig } from "next";
 
-// const nextConfig: NextConfig = {
-//   /* config options here */
-//   output: "export",
-// };
-
-// export default nextConfig;
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // This indicates you want to do static exports
-  output: 'standalone',
-  
-  // Required for GitHub Pages deployment
-  basePath: '/software-engineering-experience2024',
-  
-  // Required when deploying to GitHub Pages
-  images: {
-    unoptimized: true,
-  },
-  
-  // Required for GitHub Pages deployment
-  assetPrefix: '/software-engineering-experience2024/',
-  
-  // Disable server components for static export
-  experimental: {
-    appDir: true,
+const nextConfig: NextConfig = {
+  /* config options here */
+  output: "export",
+  images:{
+    unoptimized:true,
   }
-}
+};
 
-module.exports = nextConfig
-
+export default nextConfig;
