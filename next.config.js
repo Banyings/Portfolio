5e20721ce67d36be2208fe427aa80a9faa163642
Hyppolite Banyingela <banyingelahyppo@gmail.com>
2025-03-06 10:15:30 -0700

    lastern

---
+++ next.config.js
@@ -0,0 +1,23 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   output: "export",
+//   images:{
+//     unoptimized:true,
+//   }
+// };
+
+// export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+  /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true,
+  }
+};
+
+module.exports = nextConfig;
